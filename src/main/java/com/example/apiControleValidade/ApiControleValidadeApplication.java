4d8ebeed0917fb489a725af98bb54e380815807f
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2023-03-21 19:04:13 -0300

    criando o projeto

---
+++ src/main/java/com/example/apiControleValidade/ApiControleValidadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiControleValidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleValidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleValidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiControleValidade/ApiControleValidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiControleValidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleValidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
